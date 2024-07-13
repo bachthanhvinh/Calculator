73caa008451b0fe2add7f8b91259d89163df5a9a
Vinh <bachthanhvinh12@gmail.com>
2024-07-13 22:01:59 +0700

    calculator

---
+++ 10-calculatorcss/10-calculator.js
@@ -0,0 +1,22 @@
+let calculation = localStorage.getItem("calculation") || "";
+
+displayCalculation();
+
+function updateCalculation(value) {
+  calculation += value;
+  // console.log(calculation)
+
+  displayCalculation();
+
+  localStorage.setItem("calculation", calculation);
+}
+
+function displayCalculation() {
+  document.querySelector(".js-calculator").innerHTML = calculation;
+}
+
+// Optional: you can also create a function in order
+// to reuse this code.
+function saveCalculation() {
+  localStorage.setItem("calculation", calculation);
+}
